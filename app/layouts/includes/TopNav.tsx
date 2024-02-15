146cbef064b3a554e0d5a23b27c773c176c33b31
dsabirbaev <davran9559@gmail.com>
2024-02-15 12:28:15 +0500

    add prifle menu

--- app/layouts/includes/TopNav.tsx
+++ app/layouts/includes/TopNav.tsx
@@ -2,8 +2,13 @@
 
 import Link from "next/link";
 import { useRouter, usePathname } from "next/navigation";
-import { BiSearch, BiUser } from "react-icons/bi"
-import { AiOutlinePlus } from "react-icons/ai"
+/// iocns react
+import { BiSearch, BiUser } from "react-icons/bi";
+import { AiOutlinePlus } from "react-icons/ai";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { FiLogOut } from "react-icons/fi"
+
+
 export default function TopNav() {
     const router = useRouter();
     const pathname = usePathname();
@@ -55,6 +60,7 @@ export default function TopNav() {
                     </div>
 
                     <div className="flex items-center gap-3 "> 
+
                         <button 
                             onClick={() => goTo()} 
                             className="flex items-center border rounded-sm py-[6px] hover:bg-gray-100 pl-1.5"
@@ -63,7 +69,42 @@ export default function TopNav() {
                             <AiOutlinePlus color="#000000" size="22"/>
                             <span className="px-2 font-medium text-[15px]">Upload</span>
                         </button>
+
+
+                        {true ? (
+                            <div className="flex items-center">
+                                <button 
+                                    
+                                    className="flex items-center bg-[#F02C56] text-white border rounded-md px-3 py-[6px]"
+                                >
+                                    <span className="whitespace-nowrap mx-4 font-medium text-[15px]">Log in</span>
+                                </button>
+                                <BsThreeDotsVertical color="#161724" size="25"/>
+                            </div>
+                            ) : (
+                                <div className="flex items-center">
+                                    <div className="relative">
+                                        <button className="mt-1 border-gray-200 rounded-full">
+                                            <img className="rounded-full w-[35px] h-[35px]" src="#" />
+                                        </button>
+
+                                        <div className="absolute bg-white rounded-lg py-1.5 w-[200px] shadow-xl border top-[40px] right-0">
+                                            <button className="flex items-center w-full justify-start py-3 px-2 hover:bg-gray-100 cursor-pointer">
+                                                <BiUser size="20"/>
+                                                <span className="pl-2 font-semibold text-sm">Profile</span>
+                                            </button>
+                                            <button className="flex items-center w-full justify-start py-3 px-2 hover:bg-gray-100 cursor-pointer">
+                                                <FiLogOut size={20} />
+                                                <span className="pl-2 font-semibold text-sm">Log out</span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        }
                     </div>
+
+                   
                 </div>
             </div>
         </>
