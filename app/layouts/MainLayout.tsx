a849c4d325f854dc60680bad74c4de6eaac47415
dsabirbaev <davran9559@gmail.com>
2024-02-15 14:35:54 +0500

    done sidenav

--- app/layouts/MainLayout.tsx
+++ app/layouts/MainLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { usePathname } from "next/navigation";
 import TopNav from "./includes/TopNav";
-
+import SideNavMain from "./includes/SideNavMain";
 
 export default function MainLayout( {children} : { children: React.ReactNode }){
     const pathname = usePathname();
@@ -10,7 +10,7 @@ export default function MainLayout( {children} : { children: React.ReactNode }){
         <>
             < TopNav/>
             <div className={`flex justify-between mx-auto w-full lg:px-2.5 px-0 ${pathname == '/' ? 'max-w-[1140px]' : ''}`}>
-				
+                <SideNavMain />
 				{children}
 			</div>
         </>
---
+++ app/types.tsx
@@ -0,0 +1,19 @@
+
+
+
+export interface RandomUsers {
+    id: string;
+    name: string;
+    image: string;
+}
+
+
+export interface MenuItemTypes {
+    iconString: string,
+    colorString: string,
+    sizeString: string
+}
+
+export interface MenuItemFollowCompTypes {
+    user: RandomUsers
+}
