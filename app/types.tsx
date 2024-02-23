1b758104cf58c3764c87cf93d48e8c9e25806985
dsabirbaev <davran9559@gmail.com>
2024-02-23 11:08:15 +0500

    add custom hooks

--- app/types.tsx
+++ app/types.tsx
@@ -1,6 +1,22 @@
 
 
 
+
+export interface UserContextTypes {
+    user: User | null;
+    register: (name: string, email: string, password: string) => Promise<void>;
+    login: (email: string, password: string) => Promise<void>;
+    logout: () => Promise<void>;
+    checkUser: () => Promise<void>;
+}
+
+export interface User {
+    id: string,
+    name: string,
+    bio: string,
+    image: string,
+}
+
 export interface RandomUsers {
     id: string;
     name: string;
